4e9393a2cbe0a61992ede245cf19cc8a0bdf120f
Juan Esteban Camargo <camargo.juan@correounivalle.edu.co>
2019-05-31 11:26:49 -0500

    Modificamos el codigo, le agregamos multiplicacion division y resta

--- 2019_05_31/entero.h
+++ 2019_05_31/entero.h
@@ -4,6 +4,8 @@
 #define entero int
 #define suma(a,b) a + b
 #define resta(a,b) a - b
+#define multiplicacion(a,b) a * b
+#define division(a,b) a / b
 
 #endif
 
--- 2019_05_31/principal.c
+++ 2019_05_31/principal.c
@@ -2,9 +2,13 @@
 #include "entero.h"
 
 int main(int argc, char** argv) {
-  entero a,b;
 
-  a = 5;
-  b = 6;
-  printf("la suma de %d + %d es %d\n",a,b,suma(a,b));
+	entero a,b;
+
+	a = 5;
+	b = 6;
+	printf("la suma de %d + %d es %d\n",a,b,suma(a,b));
+	printf("la resta de %d - %d es %d \n",a,b,resta(a,b));
+	printf("la multiplicacion de %d * %d es %d \n",a,b,multiplicacion(a,b));
+	printf("la division de %d / %d es %d \n",a,b,multiplicacion(a,b));
 }
