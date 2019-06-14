fc354fa9d2b7df4c0eeec7da3a805ad2a1892aa4
invitado <camargo.juan@correounivalle.edu.c>
2019-06-14 12:37:58 -0500

    Terminamos el ejercicio completando el codigo base que nos dejo el profesor en el dia de hoy

--- 2019_06_14/mainsol.c
+++ 2019_06_14/mainsol.c
@@ -1,13 +1,28 @@
 #include "solucion.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
-  int x = 20;
 
-  printf("fibonacci(%d) = ",x);
-  x = fibonacci(x);
-  printf("%d\n",x);
-  guardarEntero("archivo",x);
+	int x = 20;
+	pid_t pid;
+	pid = fork();
 
-  return 0;
+	if(pid == 0){
+		//Codigo Hijo
+		x = fibonacci(x);
+		guardarEntero("archivo",x);
+
+	} else {
+		//Codig Padre
+		int estado;
+		int resultado;
+		wait(&estado);
+		resultado = leerEntero("archivo");
+		printf("El Fibonacci de %d es %d \n" , x , resultado*100 );
+	}
+
+	return 0;
 }
