e56c3bfaef6f4d62452d1a00e1fbd8d0962597ba
Juanescacha <camargo.juan@correounivalle.edu.co>
2019-07-09 18:37:02 -0500

    ejecutamos la shell y probamos el mensaje de bienvenida

--- Proyecto-Shell/shell.c
+++ Proyecto-Shell/shell.c
@@ -11,14 +11,28 @@
 #define MAXARGS 100 // numero maximo de comandos suportados
 #define clear() printf("\033[H\033[J")
 
+// Inicio de Shell
+
+void init_shell() {
+
+	clear();
+	printf("\n\n\n Bienvenidos a nuestra Shell");
+	char* username = getenv("USER");
+	printf("\n\n\n  Usuario : @%s ", username);
+	printf("\n");
+	sleep(1);
+	clear();
+
+}
+
+
 int main() {
 
 	char inputString[MAXLETTERS] , *parsedArgs[MAXARGS];
 	char*parsedArgsPiped[MAXARGS];
 	int execFlag = 0;
 
-
+	init_shell();
 
 	return 0;
 }
-
