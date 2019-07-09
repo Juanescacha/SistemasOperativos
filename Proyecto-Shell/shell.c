c9f2b2ee34a507467dd46df0063debee85551945
Juanescacha <camargo.juan@correounivalle.edu.co>
2019-07-09 18:45:29 -0500

    implementamos interfaz shell

--- Proyecto-Shell/shell.c
+++ Proyecto-Shell/shell.c
@@ -9,19 +9,29 @@
 
 #define MAXLETTERS 1000 // numero maximo de letras sportado
 #define MAXARGS 100 // numero maximo de comandos suportados
-#define clear() printf("\033[H\033[J")
+#define clear() printf(" \033[H\033[J ")
 
 // Inicio de Shell
 
 void init_shell() {
 
 	clear();
-	printf("\n\n\n Bienvenidos a nuestra Shell");
+	printf(" \n\n\n Bienvenidos a nuestra Shell ");
 	char* username = getenv("USER");
-	printf("\n\n\n  Usuario : @%s ", username);
-	printf("\n");
+	printf(" \n\n\n  Usuario : @%s ", username);
+	printf(" \n ");
 	sleep(1);
 	clear();
+
+}
+
+// Mostrar Interfaz
+
+void printDir() {
+
+	char cwd[1024];
+	getcwd(cwd, sizeof(cwd));
+	printf(" \n shell=%s/myshell " , cwd);
 
 }
 
@@ -32,7 +42,16 @@ int main() {
 	char*parsedArgsPiped[MAXARGS];
 	int execFlag = 0;
 
+	// Mensaje de Bienvenida Shell
 	init_shell();
+
+	while(1) {
+
+	// Mostrar Interfaz
+	printDir();
+
+
+	}
 
 	return 0;
 }
