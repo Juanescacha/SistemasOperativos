08365a2cbade5930a3632836e38249e30f7e1427
Juanescacha <camargo.juan@correounivalle.edu.co>
2019-07-09 23:01:18 -0500

    agregamos funciones y metodos , ademas de solucion de pequenos errores

--- Proyecto-Shell/README.md
+++ Proyecto-Shell/README.md
@@ -1,3 +1,6 @@
 # Shell Basico
 
-Como Ejecutar el Shell y Compilar etc...
+para Compilar [shell.c](shell.c) usaremos el siguiente comando con sus respectivos parametros:
+'''
+gcc -Wall shell.c -o shell -lreadline
+'''
--- Proyecto-Shell/shell.c
+++ Proyecto-Shell/shell.c
@@ -31,7 +31,7 @@ void printDir() {
 
 	char cwd[1024];
 	getcwd(cwd, sizeof(cwd));
-	printf(" \n shell=%s/myshell " , cwd);
+	printf(" \n shell=%s" , cwd);
 
 }
 
@@ -40,7 +40,7 @@ void printDir() {
 int recEntrada (char* str) {
 
 	char* buf;
-	buf = readline("\n$ ");
+	buf = readline("$ ");
 
 	if (strlen(buf) != 0) {
 		add_history(buf);
@@ -61,10 +61,10 @@ void execArgs(char** parsed) {
 		printf(" \n Fallo crear Fork Hijo");
 		return;
 	} else if ( pid == 0 ) {
-		if ( execvp(parsed[0], parsed) < 0) [
+		if ( execvp(parsed[0], parsed) < 0) {
 			printf(" \n No se puedo ejecutar el comando");
 		}
-		exit(0):
+		exit(0);
 	} else {
 		// Esperar al Hijo que Termine
 		wait(NULL);
@@ -76,7 +76,7 @@ void execArgs(char** parsed) {
 void execArgsPiped(char** parsed , char** parsedpipe) {
 
 	int pipefd[2];
-	pid_t p1, p2:
+	pid_t p1, p2;
 
 	if(pipe(pipefd) < 0) {
 		printf(" \n Pipe no pudo ser inicializado");
@@ -93,7 +93,7 @@ void execArgsPiped(char** parsed , char** parsedpipe) {
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[1]);
 
-		if (execvp(parsed[0], [arsed < 0) [
+		if (execvp(parsed[0], parsed) < 0) {
 			printf(" \n No se pudo Ejecutar el comando 1");
 			exit(0);
 		}
@@ -110,8 +110,8 @@ void execArgsPiped(char** parsed , char** parsedpipe) {
 		if (p2 ==0) {
 			close(pipefd[1]);
 			dup2(pipefd[0], STDIN_FILENO);
-			close(pipefd[0]):
-			if ( execvp(parsedpipe[0], parsed pipe) < 0) {
+			close(pipefd[0]);
+			if ( execvp(parsedpipe[0], parsedpipe) < 0) {
 				printf(" \n No se pudo ejecutar el comando 2");
 				exit(0);
 			}
@@ -129,27 +129,35 @@ void abrirAyuda() {
 	"\n Lista de Comandos Soportados:"
 	"\n >cd"
 	"\n >ls"
-	"\n >exit"
-	"\n Todos los demas comandos generales disponibles en el shell de UNIX");
-
+	"\n >quit"
+	"\n >hola"
+	"\n >clr"
+	"\n >pause"
+	"\n >dir"
+	"\n >environ"
+	"\n >echo");
 	return;
 
 }
 
 int ownCmdHandler(char **parsed) {
 
-	int NoOfOwnCmds = 4, i, switchOwnArg = 0;
+	int NoOfOwnCmds = 8, i, switchOwnArg = 0;
 	char* ListOfOwnCmds [NoOfOwnCmds];
 	char* username;
 
-	ListOfOwnCmds[0] = "exit";
+	ListOfOwnCmds[0] = "quit";
 	ListOfOwnCmds[1] = "cd";
 	ListOfOwnCmds[2] = "help";
 	ListOfOwnCmds[3] = "hola";
+	ListOfOwnCmds[4] = "clr";
+	ListOfOwnCmds[5] = "pause";
+	ListOfOwnCmds[6] = "dir";
+	ListOfOwnCmds[7] = "environ";
 
-	for (i = 0, i < NoOfOwnCmds; i++) {
-		if(strcmp(parsed[0], ListOfOwnCmds[i] == 0) {
-			swuitchOwnArg = i + 1;
+	for (i = 0; i < NoOfOwnCmds; i++) {
+		if(strcmp(parsed[0], ListOfOwnCmds[i]) == 0) {
+			switchOwnArg = i + 1;
 			break;
 		}
 	}
@@ -159,7 +167,17 @@ int ownCmdHandler(char **parsed) {
 		printf("\n Adios \n");
 		exit(0);
 	case 2:
-		chdir(parsed[1]);
+		if(parsed[1] == NULL) system("pwd");
+		else {
+			char buffer[1000];
+			snprintf(buffer, sizeof(buffer), "cd %s", parsed[1]);
+			system(buffer);
+			snprintf(buffer, sizeof(buffer), "echo $? > /dev/null");
+			if(system(buffer) == 0) {
+
+				chdir(parsed[1]);
+			}
+		}
 		return 1;
 	case 3:
 		abrirAyuda();
@@ -167,6 +185,25 @@ int ownCmdHandler(char **parsed) {
 	case 4:
 		username = getenv("USER");
 		printf("\n Hola %s , usa el comando help para saber mas comandos.. \n" , username);
+		return 1;
+	case 5:
+		clear();
+		return 1;
+	case 6:
+		printf("\n Presiona Enter para continuar... \n");
+		while (1) {
+			char ch = getchar();
+			if (ch == '\n') return 1;
+		}
+	case 7:
+		if(parsed[1] == NULL) system("ls");
+		else {
+			chdir(parsed[1]);
+			system("ls");
+		}
+		return 1;
+	case 8:
+		system("printenv");
 		return 1;
 	default:
 		break;
@@ -176,15 +213,45 @@ int ownCmdHandler(char **parsed) {
 
 }
 
+int parsePipe(char* str , char** strpiped) {
+	for ( int i = 0 ; i < 2;  i++ ) {
+		strpiped[i] = strsep(&str , "|");
+		if (strpiped[i] == NULL) break;
+	}
+
+	if (strpiped[1] == NULL) return 0;
+	else return 1;
+}
+
 void parseSpace(char* str , char** parsed) {
 
-	for ( int i = 0, i < MAXARGS , i++) {
+	for ( int i = 0; i < MAXARGS; i++) {
 		parsed[i] = strsep(&str, " ");
 
-		if (parsed[i] == NULL) break:
+		if (parsed[i] == NULL) break;
 		if (strlen(parsed[i]) == 0) i--;
 	}
 
+}
+
+int processString(char* str, char** parsed, char** parsedpipe) {
+
+	char* strpiped[2];
+	int piped = 0;
+
+	piped = parsePipe(str, strpiped);
+
+	if (piped) {
+		parseSpace(strpiped[0], parsed);
+		parseSpace(strpiped[1], parsedpipe);
+	} else {
+		parseSpace(str, parsed);
+	}
+
+	if (ownCmdHandler(parsed))
+		return 0;
+	else
+		return 1 + piped;
 }
 
 int main() {
